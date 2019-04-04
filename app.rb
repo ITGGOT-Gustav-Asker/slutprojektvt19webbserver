53685feb29026ed8610e33bf82615f9bdb5dedf4
ITGGOT-Gustav-Asker <38528632+ITGGOT-Gustav-Asker@users.noreply.github.com>
2019-04-04 10:37:35 +0200

    Login

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sqlite3'
 require 'sinatra'
 require 'slim'
+# enable :sessions
 
 #Startsida
 get('/') do
@@ -20,4 +21,30 @@ post ('/create') do
     db.results_as_hash = true
     db.execute("INSERT INTO users (name, tel, email, password) VALUES (?,?,?,?)", name, tel, email, password)
     redirect('/')
+end
+
+get('/logged') do
+    slim(:logged)
+end
+
+post('/logged') do
+    email = params["email"]
+    password = params["password"]
+    db = SQLite3::Database.new("database/database.db")
+    db.results_as_hash = true
+    current = db.execute("SELECT password FROM users WHERE email is (?)", email)
+    current = current.first["password"]
+    user_id = db.execute("SELECT id FROM users WHERE email is (?)", email)
+    user_id = user_id.first["id"]
+
+
+
+#   p "user_id: #{user_id}"
+#   p "Password: #{password}"
+#   p "Current: #{current}"
+     if password == current
+        redirect('/logged')
+     else
+        redirect('/')
+     end
 end
